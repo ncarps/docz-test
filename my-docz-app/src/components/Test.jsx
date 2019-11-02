f6aac0ae4624257d1a0bb57b30347145f667c296
nat <nsebcarps09@gmail.com>
2019-11-03 03:21:58 +0800

    Testing MDX

---
+++ my-docz-app/src/components/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ButtonTest = () => {
+    return(
+        <button>Click Me!</button>
+    )
+}
+
+export const TextTest = () => {
+    return(
+        <text>Hello World!</text>
+    )
+}
